aa01d7eb7ccb9d0cf9f055d655c8cfae1f523ab0
Mert Atmaca <mertatmaca34@gmail.com>
2025-06-30 17:26:58 +0300

    ++

---
+++ Domain/Entities/Station.cs
@@ -0,0 +1,14 @@
+﻿using ISKI.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Station : BaseEntity<int>
+{
+    public string StationName { get; set; }
+    public Guid StationId { get; set; }
+}
