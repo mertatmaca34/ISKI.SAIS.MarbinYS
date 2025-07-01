f29d913432d54371134c145c42dc2c6bc8066baa
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-07-01 13:53:51 +0300

    Add Swagger support

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,8 +9,16 @@ builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("De
 
 builder.Services.AddControllers();
 builder.Services.AddHostedService<PlcDataWorker>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
