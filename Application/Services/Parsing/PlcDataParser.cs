d06e72d561a6bf7c244253e8e3e4bb54ab583075
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-07-02 09:25:46 +0300

    Refactor PlcDataParser for clarity

--- Application/Services/Parsing/PlcDataParser.cs
+++ Application/Services/Parsing/PlcDataParser.cs
@@ -12,22 +12,21 @@ public class PlcDataParser : IPlcDataParser
     public AnalogSensorData ParseAnalog(byte[] data)
     {
         // Expect sequential double values for each analog field.
-        var result = new AnalogSensorData
+        return new AnalogSensorData
         {
             Readtime = DateTime.Now,
-            AkisHizi = GetReal(data, 4),
-            Akm = GetReal(data, 36, 60),
-            CozunmusOksijen = GetReal(data, 24),
-            Debi = GetReal(data, 0, 60),
-            DesarjDebi = GetReal(data, 60, 60),
-            HariciDebi = GetReal(data, 52, 60),
-            HariciDebi2 = GetReal(data, 56, 60),
-            Koi = GetReal(data, 32),
-            Ph = GetReal(data, 16),
-            Sicaklik = GetReal(data, 40),
-            Iletkenlik = GetReal(data, 20)
+            AkisHizi = ReadReal(data, 4),
+            Akm = ReadReal(data, 36, 60),
+            CozunmusOksijen = ReadReal(data, 24),
+            Debi = ReadReal(data, 0, 60),
+            DesarjDebi = ReadReal(data, 60, 60),
+            HariciDebi = ReadReal(data, 52, 60),
+            HariciDebi2 = ReadReal(data, 56, 60),
+            Koi = ReadReal(data, 32),
+            Ph = ReadReal(data, 16),
+            Sicaklik = ReadReal(data, 40),
+            Iletkenlik = ReadReal(data, 20)
         };
-        return result;
     }
 
     public DigitalSensorData ParseDigital(byte[] data)
@@ -36,49 +35,29 @@ public class PlcDataParser : IPlcDataParser
         return new DigitalSensorData
         {
             ReadTime = DateTime.Now,
-            Kapi = GetBit(data, 0, 5),
-            Duman = GetBit(data, 0, 3),
-            SuBaskini = GetBit(data, 0, 4),
-            AcilStop = GetBit(data, 0, 7),
-            Pompa1Termik = GetBit(data, 1, 2),
-            Pompa2Termik = GetBit(data, 1, 3),
-            TemizSuTermik = GetBit(data, 1, 4),
-            YikamaTanki = GetBit(data, 1, 5),
-            Enerji = GetBit(data, 0, 6),
-            Pompa1CalisiyorMu = GetBit(data, 1, 6),
-            Pompa2CalisiyorMu = GetBit(data, 1, 7)
+            Kapi = ReadBit(data, 0, 5),
+            Duman = ReadBit(data, 0, 3),
+            SuBaskini = ReadBit(data, 0, 4),
+            AcilStop = ReadBit(data, 0, 7),
+            Pompa1Termik = ReadBit(data, 1, 2),
+            Pompa2Termik = ReadBit(data, 1, 3),
+            TemizSuTermik = ReadBit(data, 1, 4),
+            YikamaTanki = ReadBit(data, 1, 5),
+            Enerji = ReadBit(data, 0, 6),
+            Pompa1CalisiyorMu = ReadBit(data, 1, 6),
+            Pompa2CalisiyorMu = ReadBit(data, 1, 7)
         };
     }
 
-    static public double GetReal(byte[] buffer, int offset, int? divider = null)
+    public static double ReadReal(byte[] buffer, int offset, int divider = 1)
     {
-        if (divider.HasValue)
-        {
-            return Math.Round((double)(S7.GetRealAt(buffer, offset) / divider), 2);
-        }
-        else
-        {
-            return Math.Round(S7.GetRealAt(buffer, offset), 2);
-        }
+        double value = S7.GetRealAt(buffer, offset) / divider;
+        return Math.Round(value, 2);
     }
-    static public Byte GetByte(byte[] buffer, int offset)
-    {
-        return S7.GetByteAt(buffer, offset);
-    }
-    static public DateTime GetTime(byte[] buffer, int offset)
-    {
-        return S7.GetDTLAt(buffer, offset);
-    }
-    static public Boolean GetInput(byte[] buffer, int pos, int bit)
-    {
-        return S7.GetBitAt(buffer, pos, bit);
-    }
-    static public Boolean GetMB(byte[] buffer, int pos, int bit)
-    {
-        return S7.GetBitAt(buffer, pos, bit);
-    }
-    static public Boolean GetBit(byte[] buffer, int pos, int bit)
-    {
-        return S7.GetBitAt(buffer, pos, bit);
-    }
+
+    public static byte ReadByte(byte[] buffer, int offset) => S7.GetByteAt(buffer, offset);
+
+    public static DateTime ReadTime(byte[] buffer, int offset) => S7.GetDTLAt(buffer, offset);
+
+    public static bool ReadBit(byte[] buffer, int pos, int bit) => S7.GetBitAt(buffer, pos, bit);
 }
