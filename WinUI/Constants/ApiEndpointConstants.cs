ed604870f436e180afa779f874f9546918c5a43d
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-09-03 11:56:36 +0300

    Add API settings persistence

---
+++ WinUI/Constants/ApiEndpointConstants.cs
@@ -0,0 +1,9 @@
+namespace WinUI.Constants;
+
+public static class ApiEndpointConstants
+{
+    public const string ApiUrl = "api/apiendpoints";
+    public const string SavedMessage = "API ayarları kaydedildi.";
+    public const string InfoTitle = "Bilgi";
+    public const string ErrorTitle = "Hata";
+}
