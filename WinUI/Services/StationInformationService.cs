b3fa9141140c7ed2272eb5bd398069ef3f0c9754
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-09-08 11:12:06 +0300

    Use ResultStatus wrapper for station info

--- WinUI/Services/StationInformationService.cs
+++ WinUI/Services/StationInformationService.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Linq.Dynamic.Core.Tokenizer;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using WinUI.Constants;
 using WinUI.Models;
 
@@ -19,22 +17,20 @@ public class StationInformationService(HttpClient httpClient, ITicketService tic
     private readonly HttpClient _httpClient = httpClient;
     private readonly ITicketService _ticketService = ticketService;
 
-    private record StationInfoResponse(StationDto? Data);
-
     public async Task<StationDto?> GetAsync(Guid stationId)
     {
         await _ticketService.EnsureTicketAsync();
 
-        using var request = new HttpRequestMessage(HttpMethod.Post, StationConstants.StationInfoApiUrl);
+        var url = $"{StationConstants.StationInfoApiUrl}?stationId={stationId}";
+        using var request = new HttpRequestMessage(HttpMethod.Post, url);
 
+        _httpClient.DefaultRequestHeaders.Remove("AToken");
         _httpClient.DefaultRequestHeaders.Add("AToken", JsonSerializer.Serialize(new AToken { TicketId = StationConstants.Ticket }));
-
-        var body = new { stationId, ticket = StationConstants.Ticket };
-        request.Content = JsonContent.Create(body);
 
         using var response = await _httpClient.SendAsync(request);
         response.EnsureSuccessStatusCode();
-        var result = await response.Content.ReadFromJsonAsync<StationInfoResponse>();
-        return result?.Data;
+        var result = await response.Content.ReadFromJsonAsync<ResultStatus<StationDto>>();
+        return result?.objects;
     }
 }
+
