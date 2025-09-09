f674cc5fe95e7bf97ea0f74625fdb7cfe65a57e0
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-09-09 15:30:31 +0300

    Load PLC data if configured

--- WinUI/Pages/HomePage.cs
+++ WinUI/Pages/HomePage.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Nodes;
 using System.Windows.Forms;
 using WinUI.Controls.IBKS;
 using WinUI.Helpers;
@@ -44,9 +46,38 @@ namespace WinUI.Pages
             };
         }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (IsPlcConfigured())
+            {
+                TimerAssignUI_Tick(this, EventArgs.Empty);
+            }
+        }
+
         public Task<PlcDataDto?> ReadPlcDataAsync()
         {
             return _plcService.ReadAndSaveAsync();
+        }
+
+        private static bool IsPlcConfigured()
+        {
+            try
+            {
+                var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                    "..", "..", "..", "..", "Api", "appsettings.json");
+                if (!File.Exists(path))
+                    return false;
+
+                var json = File.ReadAllText(path);
+                var node = JsonNode.Parse(json);
+                var ip = node?["PlcSettings"]?["IpAddress"]?.GetValue<string>();
+                return !string.IsNullOrWhiteSpace(ip);
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
