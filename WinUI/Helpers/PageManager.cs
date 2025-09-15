efd27f4c296536fb736952d2f2315e6d63e7e301
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-09-15 09:42:07 +0300

    Add null checks to PageManager

--- WinUI/Helpers/PageManager.cs
+++ WinUI/Helpers/PageManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WinUI.Helpers;
 
@@ -10,6 +11,12 @@ public static class PageManager
 {
     public static void ShowPage(Panel panel, UserControl page)
     {
+        if (panel is null)
+            throw new ArgumentNullException(nameof(panel));
+
+        if (page is null)
+            throw new ArgumentNullException(nameof(page));
+
         panel.Controls.Clear();
         page.Dock = DockStyle.Fill;
         panel.Controls.Add(page);
