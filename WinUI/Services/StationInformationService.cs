284b087eca8736285edb9f06a54b2b840fd06aad
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-09-15 17:40:00 +0300

    use SAIS API service for station info and log send failures

--- WinUI/Services/StationInformationService.cs
+++ WinUI/Services/StationInformationService.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text.Json;
-using WinUI.Constants;
 using WinUI.Models;
 
 namespace WinUI.Services;
@@ -12,24 +8,13 @@ public interface IStationInformationService
     Task<StationDto?> GetAsync(Guid stationId);
 }
 
-public class StationInformationService(HttpClient httpClient, ITicketService ticketService) : IStationInformationService
+public class StationInformationService(ISaisApiService saisApiService) : IStationInformationService
 {
-    private readonly HttpClient _httpClient = httpClient;
-    private readonly ITicketService _ticketService = ticketService;
+    private readonly ISaisApiService _saisApiService = saisApiService;
 
     public async Task<StationDto?> GetAsync(Guid stationId)
     {
-        await _ticketService.EnsureTicketAsync();
-
-        var url = $"{StationConstants.StationInfoApiUrl}?stationId={stationId}";
-        using var request = new HttpRequestMessage(HttpMethod.Post, url);
-
-        _httpClient.DefaultRequestHeaders.Remove("AToken");
-        _httpClient.DefaultRequestHeaders.Add("AToken", JsonSerializer.Serialize(new AToken { TicketId = StationConstants.Ticket }));
-
-        using var response = await _httpClient.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-        var result = await response.Content.ReadFromJsonAsync<ResultStatus<StationDto>>();
+        var result = await _saisApiService.GetStationInformationAsync(stationId);
         return result?.objects;
     }
 }
